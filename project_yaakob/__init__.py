9a08e030f07df5da2c6ac52183da8cd31513d6c0
MosheL2680 <moshelubetski@gmail.com>
2023-10-19 21:05:01 +0300

    הוספתי לך הערות

--- project_yaakob/__init__.py
+++ project_yaakob/__init__.py
@@ -1,3 +1,7 @@
+# הקובץ הזה עושה 2 דברים:
+# 1: הופך את התיקייה הזאת מתיקייה רגילה לפקאג' של פייתון. 
+# 2:ברגע שהתוכנית ניגשת לתיקייה הזאת היא דבר ראשון נכנסת לקובץ הזה. לכן זה המקום לכל הקונפיגורציות
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,11 +9,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
-app.config['SECRET_KEY'] = "random string"
+app.config['SECRET_KEY'] = "random string" # לא הבנתי מה השורה הזאת עושה 
 
 
 db = SQLAlchemy(app)
 
 
+# ]שים לב שגם השןרה של האימפורט צריכה להיות אחרי כל הקונפיגורציה שלמעלה [אחרת אתה מייבא בלופרינט שהוא חלק מתוכנית שעוד לא הוגדרה בכלל
 from project_yaakob.customers import customers
 app.register_blueprint(customers)
